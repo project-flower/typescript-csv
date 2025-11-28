3424201722381ba354ae97203d86d530e328ab91
Yuki Nishina <project_flower@outlook.com>
2025-11-28 22:31:10 +0900

    ESLint import/order 設定

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,22 +1,51 @@
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    plugins: { import: importPlugin, js },
   },
   tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
+      "import/order": [
+        "error",
+        {
+          alphabetize: {
+            order: "asc",
+          },
+          groups: [
+            "builtin",
+            "external",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "never",
+          pathGroupsExcludedImportTypes: [],
+          pathGroups: [
+            {
+              pattern: "{react,react-dom/**,react-router/**}",
+              group: "builtin",
+              position: "before",
+            },
+          ],
+        },
+      ],
       "react/react-in-jsx-scope": "off",
     },
   },
-  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ]);
